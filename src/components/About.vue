0b258c633c3be4600d6f6154d3fe6ef3e401a167
Ivan Kiral <ivanK@kentico.com>
2022-10-14 10:57:57 +0200

    Update about

--- src/components/About.vue
+++ src/components/About.vue
@@ -49,56 +49,55 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import { defaultLanguage, initLanguageCodeObject } from '../Utilities/LanguageCodes';
 import RichTextElement from './RichTextElement.vue';
 import { Client } from '../Client.js';
+import { useI18n } from 'vue-i18n';
+import { useRoute } from 'vue-router';
+import { onMounted, ref } from 'vue';
 
-export default {
-  name: 'About',
-  props: ['language'],
-  data: () => ({
-    facts: [],
-  }),
-  computed: {
-    factsData: function () {
-      return this.facts.map(fact => ({
-        title: fact.elements.title.value,
-        descriptionElement: fact.elements.description,
-        imageLink: fact.elements.image.value[0].url,
-      }))
-    }
-  },
-  watch: {
-    language: function () {
-      this.fetchFacts();
-    }
-  },
-  methods: {
-    fetchFacts: function () {
+const { locale } = useI18n();
+const language = locale.value;
+
+let facts = [];
+const factsData = ref([]);
+
+const fetchFacts =() => {
       const factsList = initLanguageCodeObject();
       var query = Client.items().type('about_us');
 
-      if (this.language) {
-        query.languageParameter(this.language);
+      if (language) {
+        query.languageParameter(language);
       }
 
       query.toPromise()
         .then(response => {
-          if (this.language) { 
-            factsList[this.language] = response.data.items[0].elements.facts.linkedItems;
+          console.log(response)
+          if (language) { 
+            factsList[language] = response.data.items[0].elements.facts.linkedItems;
           } else {
             factsList[defaultLanguage] = response.data.items[0].elements.facts.linkedItems;
           }
-          this.facts = this.language ? factsList[this.language] : factsList[defaultLanguage];
+          facts = language ? factsList[language] : factsList[defaultLanguage];
+          factsData.value = facts.map(fact => ({
+          title: fact.elements.title.value,
+          descriptionElement: fact.elements.description,
+          imageLink: fact.elements.image.value[0].url,
+         }))
+         console.log(factsData.value)
         });
+  
+      
     }
-  },
-  mounted: function () {
-    this.fetchFacts();
-  },
-  components: {
-    RichTextElement,
-  },
-}
+
+  // watch: {
+  //   language: function () {
+  //     this.fetchFacts();
+  //   }
+  // },
+  
+  onMounted(() => {
+    fetchFacts();
+  })
 </script>
